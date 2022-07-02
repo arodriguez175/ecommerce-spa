2a96449a72910307a4f588518beac3e75ba81612
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-07-02 19:17:36 -0400

    Added a key for map to clear the missing key error.

--- src/components/Women.js
+++ src/components/Women.js
@@ -7,8 +7,8 @@ function Women() {
     <div className="women">
       <h2 className="text-center text-2xl">Women's Shoes</h2>
       <div className="grid grid-cols-3 gap-4">
-        {womensShoes.map((womensShoes) => (
-          <Link to="shoe">
+        {womensShoes.map((womensShoes, index) => (
+          <Link to="shoe" key={index}>
             <Shoe
               image={womensShoes.imageURL}
               name={womensShoes.name}
