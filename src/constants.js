9f58a140aea0d30cca195783397e311d186fee22
Anna Kurylo <akurylo@redhat.com>
2022-07-14 22:04:52 +0200

    use CSS variables and classes to change button colors
    Following this tutorial: https://css-tricks.com/color-theming-with-css-custom-properties-and-tailwind/

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const actionColor = "#363636";
 export const hoverColor = "#4d4d4d";
 export const hoverTextColor = "white";
-export const disabledActionColor = "#363636";
+export const disabledActionColor = "#555555";
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js, jsx}"],
   theme: {
+    colors: {
+      "shoe-primary": "var(--th-primary)",
+      "shoe-primary-text": "var(--th-primary-text)",
+      "shoe-hover": "var(--th-hover)",
+      "shoe-text-hover": "white",
+      "shoe-text": "var(--th-text)",
+      "shoe-option": "var(--th-secondary)",
+    },
     extend: {},
   },
   plugins: [],
