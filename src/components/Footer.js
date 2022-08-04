10321bd09f1a5e1ceddf4b161fa5efa9512369e6
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-08-04 18:21:07 -0400

    Added footer content and resized bottom footer text.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,48 @@
 function Footer() {
   return (
     <div className="footer">
-      <div className="bottom-0 text-center">
-        <p>footer</p>
+      <div className="bottom-0 grid grid-cols-4 mb-8">
+        <div>
+          <p>GIFT CARDS</p>
+          <p>PROMOTIONS</p>
+          <p>FIND A STORE</p>
+          <p>SIGN UP FOR EMAIL</p>
+          <p>BECOME A MEMBER</p>
+          <p>SEND US FEEDBACK</p>
+        </div>
+
+        <div>
+          <p>GET HELP</p>
+          <p>Order Status</p>
+          <p>Shipping and Delivery</p>
+          <p>Returns</p>
+          <p>Payment Options</p>
+          <p>Gift Card Balance</p>
+          <p>Contact Us</p>
+        </div>
+
+        <div>
+          <p>ABOUT MERCURY</p>
+          <p>News</p>
+          <p>Careers</p>
+          <p>Purpose</p>
+          <p>Sustainability</p>
+        </div>
+
+        <div>{/* social media icons */}</div>
+      </div>
+
+      <div className="footerBottom">
+        <p className="float-left">
+          &copy; {new Date().getFullYear()} Mercury, Inc. All Rights Reserved
+        </p>
+
+        <div className="footerBottomRight">
+          <p>Guides</p>
+          <p>Terms of Sale</p>
+          <p>Terms of Use</p>
+          <p>Mercury Privacy Policy</p>
+        </div>
       </div>
     </div>
   );
