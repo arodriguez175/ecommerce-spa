1f28712b23aa63ab9b8f66ea8569a43f885f3151
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-08-08 16:06:53 -0400

    Made searchbar have an autofill feature.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,30 @@
+import { useSelector } from "react-redux";
+import Turnstone from "turnstone";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
 function Search() {
+  const shoeCatalog = useSelector((state) => state.shoeCatalog.shoes);
+  const navigate = useNavigate();
+  const turnstoneRef = useRef();
+  const listBox = {
+    displayField: "name",
+    data: shoeCatalog,
+  };
+
   return (
     <div className="search">
       <span>
-        <input
+        <Turnstone
+          listbox={listBox}
+          typeahead={true}
+          ref={turnstoneRef}
+          onSelect={(selectedItem) => {
+            if (selectedItem) {
+              navigate(`/shoes/${selectedItem.id}`);
+              turnstoneRef.current?.clear();
+            }
+          }}
           className="border border-lightgrayBorder shadow 1px 2px 3px gainsboro-100 rounded p-3 w-[600px]"
           placeholder="Search for..."
         />
