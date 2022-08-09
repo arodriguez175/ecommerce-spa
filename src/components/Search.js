9a92170fe12918af1077e3d287029e93c663c807
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-08-08 22:03:52 -0400

    Fixed and finished the search bar.

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,31 +7,42 @@ function Search() {
   const shoeCatalog = useSelector((state) => state.shoeCatalog.shoes);
   const navigate = useNavigate();
   const turnstoneRef = useRef();
+
   const listBox = {
     displayField: "name",
     data: shoeCatalog,
+    searchType: "startsWith",
+  };
+
+  const styles = {
+    input:
+      "border border-lightgrayBorder shadow 1px 2px 3px rounded p-3 w-[600px] focus:outline-none",
+    listbox:
+      "w-full border border-[#f6f6f6] bg-secondary rounded text-left mt-2 p-3 [&>*]:leading-8 drop-shadow-xl",
+    highlightedItem:
+      "cursor-pointer bg-[#f6f6f6] overflow-ellipsis overflow-hidden",
   };
 
   return (
     <div className="search">
-      <span>
+      <div className="flex">
         <Turnstone
           listbox={listBox}
           typeahead={true}
           ref={turnstoneRef}
+          styles={styles}
           onSelect={(selectedItem) => {
             if (selectedItem) {
               navigate(`/shoes/${selectedItem.id}`);
               turnstoneRef.current?.clear();
             }
           }}
-          className="border border-lightgrayBorder shadow 1px 2px 3px gainsboro-100 rounded p-3 w-[600px]"
           placeholder="Search for..."
         />
         <button className="bg-primary text-secondary ml-1 rounded p-3 hover:bg-darkgray">
           Search
         </button>
-      </span>
+      </div>
     </div>
   );
 }
