68874c5d3b92ff73ccd0a18b223eb71615470767
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-08-10 00:16:32 -0400

    Resized bag icon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,7 +66,7 @@ function Header() {
           to="/cart"
           className="hover:border border-lightgrayBorder hover:m-[-1px] rounded p-3"
         >
-          <FontAwesomeIcon icon={faBagShopping} />
+          <FontAwesomeIcon icon={faBagShopping} className="text-[25px]" />
           <span className="bg-red text-secondary rounded-full min-w-[1rem] w-auto text-center text-[0.7rem] absolute">
             {cartLength > 0 ? cartLength : ""}
           </span>
