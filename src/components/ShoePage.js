b66b47f897d43248e4dcf5116b92ace774ee3178
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-09-09 17:01:27 -0400

    Made shoe page responsive for mobile and tablet screens.

--- src/components/ShoePage.js
+++ src/components/ShoePage.js
@@ -41,20 +41,26 @@ function ShoePage() {
   };
 
   return (
-    <div className="mx-20 mt-10">
+    <div className="mx-4 md:mx-8 md:mt-3 lg:mx-20 lg:mt-10">
       {shoe ? (
-        <div className="flex">
+        <div className="md:flex">
           <div>
             <img
               src={shoe.imageURL}
-              className="drop-shadow-md rounded-xl w-[90%]"
+              className="drop-shadow-md rounded-xl w-full md:w-[90%]"
             />
           </div>
 
           <div>
-            <p className="text-[28pt]">{shoe.name}</p>
-            <p className="text-[16pt]">{shoe.gender}</p>
-            <p className="text-[16pt]">${shoe.price}</p>
+            <p className="text-[20pt] md:text-[22pt] lg:text-[28pt]">
+              {shoe.name}
+            </p>
+            <p className="text-[13pt] md:text-[14pt] lg:text-[16pt]">
+              {shoe.gender}
+            </p>
+            <p className="text-[13pt] md:text-[14pt] lg:text-[16pt]">
+              ${shoe.price}
+            </p>
 
             <p className=" mt-5 mb-2">Select Size</p>
             <div className="grid grid-cols-5 gap-1 cursor-pointer">
@@ -62,7 +68,7 @@ function ShoePage() {
                 return (
                   <label
                     key={size}
-                    className={`border border-lightgrayBorder text-center rounded p-2 px-5 hover:bg-primary hover:text-secondary hover:cursor-pointer 
+                    className={`border border-lightgrayBorder text-center rounded p-2 md:px-5 hover:bg-primary hover:text-secondary hover:cursor-pointer 
                     ${size === selectedSize ? "selected" : ""}`}
                   >
                     {size}
@@ -83,10 +89,10 @@ function ShoePage() {
               })}
             </div>
 
-            <div className="border border-lightgrayBorder mt-5 justify-center rounded grid w-[400px]">
+            <div className="border border-lightgrayBorder mt-5 justify-center rounded grid md:w-[400px]">
               <button
                 onClick={handleAddToCart}
-                className={`bg-primary text-secondary hover:text-secondary hover:bg-darkgray w-[300px] p-2 mt-3 mb-3 rounded
+                className={`bg-primary text-secondary hover:text-secondary hover:bg-darkgray w-[250px] md:w-[300px] p-2 mt-3 mb-3 rounded
                 ${!selectedSize && "disabled"}
                 `}
                 disabled={!selectedSize}
@@ -95,7 +101,7 @@ function ShoePage() {
               </button>
               <button
                 onClick={handleSaveItem}
-                className="border border-lightgrayBorder w-[300px] p-2 mb-3 rounded"
+                className="border border-lightgrayBorder w-[250px] md:w-[300px] p-2 mb-3 rounded"
               >
                 Save to List
               </button>
