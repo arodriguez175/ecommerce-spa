806af0c11ef33b1ca95252e816d6b7bbd6f8428d
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-09-12 22:31:35 -0400

    Made saved page responsive for mobile screens.

--- src/components/SavedShoePage.js
+++ src/components/SavedShoePage.js
@@ -9,7 +9,7 @@ function SavedShoePage() {
   return (
     <div>
       <h2 className="text-center text-2xl m-5">Saved Items</h2>
-      <div className="grid grid-cols-3 gap-10 mx-20">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-10 md:mx-20">
         {savedItems.map((item, index) => (
           <SavedShoeCard
             key={index}
