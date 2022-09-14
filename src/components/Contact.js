551e344bd2100e95885a45b45acf60a75cd8a6b0
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-09-14 15:31:22 -0400

    Finished Contact page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div className="mx-4 md:mx-[115px] lg:mx-[230px] mt-5 text-center border border-lightgrayBorder rounded-xl [&>*]:m-3">
+      <h2 className="text-center text-2xl">Contact Us</h2>
+      <p>1-800-123-1234</p>
+      <p>5am-12pm</p>
+      <p>Mon-Sun</p>
+      <p>
+        <a href="mailto:mercurystore@example.com">mercurystore@example.com</a>
+      </p>
     </div>
   );
 }
