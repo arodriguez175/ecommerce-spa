b46493b32e020323585558ca81aad76dbc61a500
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-09-15 22:11:38 -0400

    Added project GitHub icon link.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Layout() {
   return (
     <div>
       <div className="pt-1 px-1 md:pt-6 md:px-6">
+        <a
+          href="https://github.com/arodriguez175/ecommerce-spa"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className="text-[25px] text-primary"
+          />
+        </a>
         <Header />
         <Outlet />
       </div>
